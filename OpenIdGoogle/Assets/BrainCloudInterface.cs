bd2883d9256826c768c988a7c3a4b3bac6c06236
ryanruth <ryanr@bitheads.com>
2020-10-27 17:49:04 -0400

    Adding google purchase functionality

--- OpenIdGoogle/Assets/BrainCloudInterface.cs
+++ OpenIdGoogle/Assets/BrainCloudInterface.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,8 +8,9 @@ using System.Threading.Tasks;
 //Need to use these for google
 using Google.Impl;
 using Google;
+using UnityEngine.Purchasing;
 
-public class BrainCloudInterface : MonoBehaviour
+public class BrainCloudInterface : MonoBehaviour, IStoreListener //needed for unity iap
 {
     //these are simply references to the unity specific canvas system
     Text status;
@@ -20,6 +22,11 @@ public class BrainCloudInterface : MonoBehaviour
     GoogleSignInConfiguration configuration;
     //the webClientId of our googleOpenId test app. To test your own app, enter in your apps own webClientId
     string webClientId = "780423637529-hcg9gdbo4egbbh7h6k9pkukd8a9j9f8u.apps.googleusercontent.com";
+
+    //for purchasing
+    private static IStoreController m_StoreController;          // The Unity Purchasing system.
+    private static IExtensionProvider m_StoreExtensionProvider; // The store-specific Purchasing subsystems.
+    public static string kProductIDConsumable = "com.bitheads.OpenIdGoogle.bc_google_orb1";
 
     // Use this for initialization
     void Start()
@@ -38,13 +45,24 @@ public class BrainCloudInterface : MonoBehaviour
             RequestIdToken = true,
             //auth code is not needed for OpenId authentication
             RequestAuthCode = true
-        };     
+        };
+
+
+        // If we haven't set up the Unity Purchasing reference
+        if (m_StoreController == null)
+        {
+            // Begin to configure our connection to Purchasing
+            InitializePurchasing();
+        }
+
     }
 
     void Update()
     {
         status.text = statusText;
     }
+
+    //google sign in
 
     public void OnGoogleSignIn()
     {
@@ -118,9 +136,41 @@ public class BrainCloudInterface : MonoBehaviour
         statusText = "Signed out of Google";
     }
 
+    //purchasing 
+
+    public void InitializePurchasing()
+    {
+        // If we have already connected to Purchasing ...
+        if (IsInitialized())
+        {
+            // ... we are done here.
+            return;
+        }
+
+        // Create a builder, first passing in a suite of Unity provided stores.
+        var configurationBuilder = ConfigurationBuilder.Instance(Google.Play.Billing.GooglePlayStoreModule.Instance()); //For Google purchasing specifically
+        //var configurationBuilder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+
+        // Add a product to sell / restore by way of its identifier, associating the general identifier
+        // with its store-specific identifiers.
+        configurationBuilder.AddProduct(kProductIDConsumable, ProductType.Consumable);
+
+        // Kick off the remainder of the set-up with an asynchrounous call, passing the configuration 
+        // and this class' instance. Expect a response either in OnInitialized or OnInitializeFailed.
+        UnityPurchasing.Initialize(this, configurationBuilder);
+    }
+
+    private bool IsInitialized()
+    {
+        // Only say we are initialized if both the Purchasing references are set.
+        return m_StoreController != null && m_StoreExtensionProvider != null;
+    }
+
     public void OnGooglePurchase()
     {
-
+        // Buy the consumable product using its general identifier. Expect a response either 
+        // through ProcessPurchase or OnPurchaseFailed asynchronously.
+        BuyProductID(kProductIDConsumable);
     }
 
     public void OnVerifyPurchase()
@@ -128,4 +178,109 @@ public class BrainCloudInterface : MonoBehaviour
 
     }
 
+    void BuyProductID(string productId)
+    {
+        // If Purchasing has been initialized ...
+        if (IsInitialized())
+        {
+            // ... look up the Product reference with the general product identifier and the Purchasing 
+            // system's products collection.
+            Product product = m_StoreController.products.WithID(productId);
+
+            // If the look up found a product for this device's store and that product is ready to be sold ... 
+            if (product != null && product.availableToPurchase)
+            {
+                Debug.Log(string.Format("Purchasing product asychronously: '{0}'", product.definition.id));
+                // ... buy the product. Expect a response either through ProcessPurchase or OnPurchaseFailed 
+                // asynchronously.
+                m_StoreController.InitiatePurchase(product);
+            }
+            // Otherwise ...
+            else
+            {
+                // ... report the product look-up failure situation  
+                Debug.Log("BuyProductID: FAIL. Not purchasing product, either is not found or is not available for purchase");
+            }
+        }
+        // Otherwise ...
+        else
+        {
+            // ... report the fact Purchasing has not succeeded initializing yet. Consider waiting longer or 
+            // retrying initiailization.
+            Debug.Log("BuyProductID FAIL. Not initialized.");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //  
+    // --- IStoreListener callbacks
+    //
+
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        // Purchasing has succeeded initializing. Collect our Purchasing references.
+        statusText = "OnInitialized: Google Store PASS";
+        //Debug.Log("OnInitialized: PASS");
+
+        // Overall Purchasing system, configured with products for this application.
+        m_StoreController = controller;
+        // Store specific subsystem, for accessing device-specific store features.
+        m_StoreExtensionProvider = extensions;
+    }
+
+
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        // Purchasing set-up has not succeeded. Check error for reason. Consider sharing this reason with the user.
+        statusText = "OnInitializeFailed InitializationFailureReason:" + error;
+        statusText = "blah bala";
+        //Debug.Log("OnInitializeFailed InitializationFailureReason:" + error);
+    }
+
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
+    {
+        // A consumable product has been purchased by this user.
+        if (String.Equals(args.purchasedProduct.definition.id, kProductIDConsumable, StringComparison.Ordinal))
+        {
+            //Debug.Log(string.Format("ProcessPurchase: PASS. Product: '{0}'", args.purchasedProduct.definition.id));
+            statusText = "ProcessPurchase: PASS. Product: " + args.purchasedProduct.definition.id;
+        }
+        else
+        {
+            //Debug.Log(string.Format("ProcessPurchase: FAIL. Unrecognized product: '{0}'", args.purchasedProduct.definition.id));
+            statusText = "ProcessPurchase: FAIL. Unrecognized product: " + args.purchasedProduct.definition.id;
+        }
+
+        // Return a flag indicating whether this product has completely been received, or if the application needs 
+        // to be reminded of this purchase at next app launch. Use PurchaseProcessingResult.Pending when still 
+        // saving purchased products to the cloud, and when that save is delayed. 
+        return PurchaseProcessingResult.Complete;
+    }
+
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        // A product purchase attempt did not succeed. Check failureReason for more detail. Consider sharing 
+        // this reason with the user to guide their troubleshooting actions.
+        //Debug.Log(string.Format("OnPurchaseFailed: FAIL. Product: '{0}', PurchaseFailureReason: {1}", product.definition.storeSpecificId, failureReason));
+        statusText = "OnPurchaseFailed: FAIL. Product: " + product.definition.storeSpecificId + ", PurchaseFailureReason: " + failureReason;
+    }
 }
